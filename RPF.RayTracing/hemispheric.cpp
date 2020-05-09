fce44ef24ad731fe39d9e476771fedc0f6352627
Hugo Zammit <hugo.zammit@signature-code.com>
2020-05-09 13:35:48 +0200

    [WIP] Full solution cleanup.

--- RPF.RayTracing/hemispheric.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "hemispheric.h"
-
-bool ray_tracing::material::hemispheric::scatter(const core::ray& r, const core::hit_record& record, color& attenuation, core::ray& scattered) const
-{
-	const auto scatter_direction = random_in_hemisphere(record.normal);
-	scattered = core::ray(record.position, scatter_direction);
-	attenuation = albedo;
-	return true;
-}
