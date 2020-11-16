127830afe0d7e3e6caf4a75074b42f8ab26340ff
Hugo Zammit <hugo.zammit@signature-code.com>
2020-11-16 06:58:56 +0100

    Add axis-aligned bounding rectangular parallelepiped implementation.

---
+++ RPF.RayTracing/core/include/aabb.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#ifndef RPF_RAYTRACING_CORE_AABB_H
+#define RPF_RAYTRACING_CORE_AABB_H
+
+#include "core/include/common.h"
+#include "core/include/ray.h"
+
+namespace ray_tracing
+{
+	namespace core
+	{
+		class aabb
+		{
+		public:
+			aabb() = default;
+
+			aabb(const point3& a, const point3& b);
+
+			static aabb bbox(aabb a, aabb b);
+			
+			bool hit(const ray& r, float t_min, float t_max) const;
+
+		private:
+			point3 min_;
+			point3 max_;
+
+		public:
+			point3 min() const { return min_; }
+			point3 max() const { return max_; }
+		};
+	}
+}
+
+#endif
---
+++ RPF.RayTracing/core/src/aabb.cpp
@@ -0,0 +1,41 @@
+﻿#include "core/include/aabb.h"
+
+ray_tracing::core::aabb::aabb(const point3& a, const point3& b)
+	: min_(a), max_(b) {}
+
+bool ray_tracing::core::aabb::hit(const ray& r, float t_min, float t_max) const
+{
+	for (auto i = 0; i < 3; ++i)
+	{
+		const auto inverted_direction = 1.F / r.direction().a[i];
+		auto t0 = (min().a[i] - r.origin().a[i]) * inverted_direction;
+		auto t1 = (max().a[i] - r.origin().a[i]) * inverted_direction;
+		if (inverted_direction < 0.F) std::swap(t0, t1);
+
+		t_min = t0 > t_min ? t0 : t_min;
+		t_max = t1 < t_max ? t1 : t_max;
+
+		if (t_max <= t_min) return false;
+	}
+
+	return true;
+}
+
+ray_tracing::core::aabb ray_tracing::core::aabb::bbox(aabb a, aabb b)
+{
+	const point3 min =
+	{
+		fminf (a.min_.a[0], b.min_.a[0]),
+		fminf(a.min_.a[1], b.min_.a[1]),
+		fminf(a.min_.a[2], b.min_.a[2])
+	};
+
+	const point3 max =
+	{
+		fmaxf(a.max_.a[0], b.max_.a[0]),
+		fmaxf(a.max_.a[1], b.max_.a[1]),
+		fmaxf(a.max_.a[2], b.max_.a[2])
+	};
+
+	return {min, max};
+}
